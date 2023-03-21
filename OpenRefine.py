91d3ba9f651544785d23be403c40781b084ac54f
nikoshourdas <nikos.hourdas@gmail.com>
2023-03-21 16:16:17 +0200

    facet request successful in this iteration

--- OpenRefine.py
+++ OpenRefine.py
@@ -1,1 +1,33 @@
-# ?init 
+import json 
+import requests 
+
+project_url = 'http://localhost:3333'
+
+project_id = 1866105579170
+
+
+# make a facet request that sorts by id number 
+facet = {
+    "op": "core/column-facet",
+    "column": "id_number",
+    "expression": "value.toNumber()",
+    "facet": {
+        "type": "list",
+        "name": "id_number_facet",
+        "choiceCount": 50
+    }
+}
+# Send the request to OpenRefine
+response = requests.post(
+    f"{project_url}",
+    data={
+        'project': project_id,
+        'operations': json.dumps([facet])
+    }
+)
+
+if response.status_code == 200: 
+    print("facet request successful")
+
+else : 
+    print(f"Facet request failed with status code {response.status_code}")
