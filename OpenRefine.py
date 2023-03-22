38546d2c229334609edb7e2db4efed137b132981
nikoshourdas <nikos.hourdas@gmail.com>
2023-03-22 10:54:03 +0200

    base code for test

--- OpenRefine.py
+++ OpenRefine.py
@@ -1,1 +1,19 @@
-# ?init 
+import requests
+import json 
+
+url = 'http://localhost:3333/command/core'
+
+payload = { 
+           'project' : '1866105579170',
+           'commands' : [
+               {
+                   'operations' : 'core/column-rename',
+                   'oldColumnName': 'id',
+                   'newColumnName': 'newId'
+               }
+           ]
+}
+
+response = requests.post(url , data = json.dumps(payload))
+
+print(response.content) 
