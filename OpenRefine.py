dbb09e099d3f3eeb0e576f8208bffd633c4b8d3e
nikoshourdas <nikos.hourdas@gmail.com>
2023-03-22 11:08:49 +0200

    comment

--- OpenRefine.py
+++ OpenRefine.py
@@ -1,14 +1,17 @@
 import requests
 import json 
 
-url = 'http://localhost:3333/command/core'
+url = 'http://localhost:3333/command/core' 
+# /command/core
+# we use this end point to automate the data manupulation tasks we want to process 
+
 
 payload = { 
-           'project' : '1866105579170',
+           'project' : '2318446209232',
            'commands' : [
                {
                    'operations' : 'core/column-rename',
-                   'oldColumnName': 'id',
+                   'oldColumnName': 'product-id',
                    'newColumnName': 'newId'
                }
            ]
