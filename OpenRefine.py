a19eef0d5213de5a14dd5db76bdbbaf81751f9ad
nikoshourdas <nikos.hourdas@gmail.com>
2023-03-22 11:53:17 +0200

    most commented out / csrf token retrieval

--- OpenRefine.py
+++ OpenRefine.py
@@ -1,22 +1,40 @@
 import requests
 import json 
 
-url = 'http://localhost:3333/command/core' 
+url = 'http://localhost:3333' 
 # /command/core
 # we use this end point to automate the data manupulation tasks we want to process 
 
+# project_ID = '2318446209232'
+# column_name = 'product-id'
 
-payload = { 
-           'project' : '2318446209232',
-           'commands' : [
-               {
-                   'operations' : 'core/column-rename',
-                   'oldColumnName': 'product-id',
-                   'newColumnName': 'newId'
-               }
-           ]
-}
+# payload = { 
+#            'project' : '2318446209232',
+#            'commands' : [
+#                {
+#                    'operations' : 'core/column-rename',
+#                    'oldColumnName': 'product-id',
+#                    'newColumnName': 'newId'
+#                }
+#            ]
+# }
 
-response = requests.post(url , data = json.dumps(payload))
+csrf_token = requests.get(url + '/csrf_token')
+print(csrf_token.text)
 
-print(response.content) 
+# payload = {
+#     'projectID': project_ID,
+#     'columnName': column_name,
+#     'expression': f'value.replace("product-id", "new_label")'
+# }
+# headers = {'Content-Type': 'application/json'}
+
+# response = requests.post(url, data=json.dumps(payload), headers=headers)
+
+
+# print(response.status_code)
+# Parse the JSON response
+# response_json = json.loads(response.content)
+
+# Print the response status
+# print(response_json['code'])
